9c03bb2ea8dc379a306482bc5cac56fc5fd48edc
Matthias Wahl <matthiaswahl@m7w3.de>
2021-03-21 22:41:37 +0100

    Adding more and more terms.

--- grammar.js
+++ grammar.js
@@ -1,11 +1,15 @@
 module.exports = grammar({
-  name: 'ponylang',
+    name: 'ponylang',
 
   extras: $ => [/\s|\\\n/, $.line_comment, $.block_comment],
   externals: $ => [
     $.block_comment,
     $.string
   ],
+  supertypes: $ => [
+    $._term,
+  ],
+  word:            $ => $.identifier,
   rules: {
     source_file :  $ => seq(optional($.string), repeat($.use)/*TODO: , repeat($.class_def)*/),
     line_comment:  $ => token(seq('//', /.*/)),
@@ -17,31 +21,157 @@ module.exports = grammar({
                          optional(field('typeargs', $.typeargs)),
                          optional(field('cap',$._type_cap ))
                        ),
-    _infix_type:   $ => seq($.type, repeat(seq(choice('|', '&'), $.type))),
-    _grouped_type: $ => seq('(', $._infix_type, repeat(seq(',', $._infix_type)), ')'),
-    _lambda_type:  $ => seq(
+    union_type:     $ => seq($._inner_type, repeat1(seq('|', $._inner_type))),
+    isect_type:     $ => seq($._inner_type, repeat1(seq('&', $._inner_type))),
+    tuple_type:     $ => seq($._inner_type, repeat1(seq(',', $._inner_type))),
+    _grouped_type:  $ => seq(
+        '(', 
+        choice(
+            $._inner_type,
+            $.union_type,
+            $.isect_type,
+            $.tuple_type
+        ), 
+        ')'
+    ),
+    lambda_type:    $ => seq(
         choice('{', '@{'), 
         optional($.cap),
         optional(field('name', $.identifier)),
         optional(field('typeparams', $.typeparams)),
         '(',
-        seq($.type, repeat(seq(',', $.type))),
+        seq($._inner_type, repeat(seq(',', $._inner_type))),
         ')',
-        optional(seq(':', field('return_type', $.type))),
+        optional(seq(':', field('return_type', $._inner_type))),
         optional($.partial),
         '}', 
         optional($._type_cap)
     ),
-    type:         $ => seq(
-                         choice('this', $.cap, $._nominal_type, $._grouped_type, $._lambda_type),
-                         optional(field('viewpoint', seq('->', $.type)))
+    _inner_type:    $ => seq(
+                         choice('this', $.cap, $._nominal_type, $._grouped_type, $.lambda_type),
+                         optional(field('viewpoint', seq('->', $._inner_type)))
     ),
-    asop:         $ => seq('as', $.type),
-    term:         $ => 'TODO', // TODO
-    binop:        $ => seq(choice('+', '-'), $.term), // TODO: more operators
-    infix:        $ => seq($.term, repeat(choice($.binop, $.asop))),
-    identifier:   $ => seq(/[a-zA-Z_]/, repeat(/[a-zA-Z0-9_']/)),
-    param:        $ => seq($.identifier, ':', $.type, optional(seq('=', $.infix))),
+    type:           $ => $._inner_type,
+    asop:           $ => prec(2, seq(
+        field('lhs', $._term),
+        'as',
+        field('rhs', $.type))
+    ),
+    consume:        $ => seq('consume', optional($.cap), $._term),
+    // jumps
+    "return":       $ => seq('return', optional($.block)),
+    "break":        $ => seq('break', optional($.block)),
+    "continue":     $ => seq('continue', optional($.block)),
+    "error":        $ => seq('error', optional($.block)),
+    "compile_intrinsic":  $ => seq('compile_intrinsic', optional($.block)),
+    "compile_error":  $ => seq('compile_error', optional($.block)),
+    _jump:        $ => choice(
+            $.return,
+            $.break,
+            $.continue,
+            $.error,
+            $.compile_intrinsic,
+            $.compile_error
+    ),
+    assignment:   $ => seq(
+        $._infix,
+        optional(
+            seq(
+                '=',
+                $.assignment
+            )
+        )
+    ),
+    _block_exprs:       $ => seq(
+        $.assignment,
+        repeat(seq(optional(';'), $.assignment))
+    ),
+    block:     $ => choice(
+        seq($._block_exprs, optional($._jump)),
+        $._jump
+    ),
+    annotations:  $ => seq('\\', $.identifier, repeat(seq(',', $.identifier))),
+    recover:      $ => seq('recover', optional($.annotations), optional($.cap), $.block, 'end'),
+    elseif:       $ => seq(
+        'elseif',
+        optional($.annotations),
+        field('condition', $.block),
+        'then',
+        field('if_block', $.block)
+    ),
+    "if":         $ => seq(
+        'if',
+        optional($.annotations),
+        field('condition', $.block),
+        'then',
+        field('if_block', $.block),
+        repeat(
+            $.elseif
+        ),
+        optional(
+            seq('else', field('else_block', $.block)),
+        ),
+        'end'
+    ),
+    local:        $ => seq(
+        choice('var', 'let', 'embed'),
+        $.identifier,
+        optional(
+            seq(
+                ':',
+                $.type
+            )
+        )
+    ),
+    _term:        $ => choice(
+        // TODO: add more
+        $.identifier,
+        $.local,
+        $.if,
+        $.consume,
+        $.recover
+    ),
+    _partial_ops: $ => choice(
+                            'and',
+                            'or',
+                            'xor',
+                            '+', 
+                            '-',
+                            '/',
+                            '*',
+                            '%',
+                            '%%',
+                            '+~',
+                            '-~',
+                            '/~',
+                            '*~',
+                            '%~',
+                            '%%~',
+                            '>>',
+                            '<<',
+                            '>>~',
+                            '<<~',
+                            '==',
+                            '!=',
+                            '>',
+                            '<',
+                            '>=',
+                            '<='),
+    binop:        $ => prec(1, seq(
+        field('lhs', $._term), 
+        choice(
+            seq($._partial_ops, optional($.partial)),
+            choice('is', 'isnt')
+        ),
+        field('rhs', $._term)
+    )),
+    _infix:       $ => choice(
+        $._term,
+        $.binop,
+        $.asop
+    ),
+    identifier:   $ => token(seq(/[a-zA-Z_]/, repeat(/[a-zA-Z0-9_']/))),
+    param:        $ => seq($.identifier, ':', $.type, optional(seq('=', $._infix))),
     params:       $ => seq($.param, repeat(seq(',', $.param))),
     typeparam:    $ => seq(
         field('name', $.identifier), 
@@ -61,6 +191,6 @@ module.exports = grammar({
                         'use', 
                         optional(field('name', $._use_name)), 
                         field('specifier', choice($.string, $._use_ffi)), 
-                        optional(field('condition', $.infix))), 
+                        optional(seq('if', field('condition', $._infix)))), 
   }
 });
